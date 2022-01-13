f87bf4867e4f614db37db3599a6b9a323e19d7af
Reid Erwin <reidmmg42@gmail.com>
2022-01-13 15:27:36 -0500

    Added comments in code

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,8 +12,10 @@ profit_loss = []
 with open(financial_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    # Read the header row first
     csvheader = next(csvreader)
 
+    # Read through each row of data after the header
     for row in csvreader:
 
         #Add date to python list
@@ -22,31 +24,46 @@ with open(financial_data) as csvfile:
         #Add profit_loss to python list
         profit_loss.append(int(row[1]))
 
+    #New variable to hold value for total number of rows (months) in the date list.
     total_months = len(date)
+
+    #New varialble to hold the sum of "profit_loss" list.  Formats to dollars.
     total_profit_loss = "${}".format(sum(profit_loss))
     
+    #New placeholder to store list of differences in profit loss for the next loop.
     difference_list = []
     
+    #Loop to compare adjacent values in "profit_loss" list
     for row in range(total_months - 1):
         first_number = profit_loss[row]
         second_number = profit_loss[row+1]
 
+        #New variable to calculate difference between adjacent values
         difference = second_number - first_number
+        
+        #Appends placeholder list with the results of the calculation.
         difference_list.append(difference)
 
+    #New variable generated from calculation which determines average of difference of adjacent values.
+    #Formatted for dollars and cents
     average = "${:,.2f}".format((sum(difference_list)) / len(difference_list))
     
+    #New variable to hold max value in "difference_list."
     greatest_increase = max(difference_list)
+    #New variable to hold index of greatest increase in "difference_list."
     greatest_increase_index = difference_list.index(greatest_increase)
+    #New variable to hold date using index from previous step.
     greatest_date = date[greatest_increase_index + 1]
+    #Formats for dollars
     greatest_increase = "${}".format(greatest_increase)
 
+    #Same operations as above, except for decreases.
     greatest_decrease = min(difference_list)
     greatest_decrease_index = difference_list.index(greatest_decrease)
     greatest_decrease_date = date[greatest_decrease_index + 1]
     greatest_decrease = "${}".format(greatest_decrease)
 
-
+    #F strings to print results as requested.
     print("Financial Analysis")
     print("---------------------------")
     print(f'Total Months: {total_months}')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,8 +11,10 @@ candidate = []
 with open(election_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    # Read the header row first
     csvheader = next(csvreader)
 
+    # Read through each row of data after the header
     for row in csvreader:
 
         #Add column index 0, "vote id" from csv file to python list
@@ -21,27 +23,43 @@ with open(election_data) as csvfile:
         #Adds column index 2, "candidate" from csv file to python list
         candidate.append(row[2])
 
+    #New variable.
+    #Determines total number of votes via length of voter_id list & converts to integer
     total_votes = int(len(voter_id))
 
-    
+    #New varialbe.
+    #Function creates a list of 2 new lists. The first list is a unique set of the names.  
+    #The second list counts the amount of times the name in the first list appears.
+    #Note, I did reference this from the interwebs, but felt it was pretty boilerplate, so fair game.
     candidate_count = [[j,candidate.count(j)] for j in set(candidate)]
 
-
+    #New variables.
+    #Since "candidate_count" is a list of 2 lists, the following separates the lists into standalone list.
+    #Both lists contain 4 entries with this particular dataset since there are 4 unique names.
+    #This enables soft coding for a similar dataset of election data.
     name_list = [i[0] for i in candidate_count]
     count_list = [i[1] for i in candidate_count]
 
+    #New variable to hold max value from variable "count_list"
     max_value = max(count_list)
 
+    #New variable to determine index where the max value occurs in "count_list"
     winner_index = count_list.index(max_value)
     
+    #New variable to store name of winning cadidate using the index from the previous step.
     winner_name = name_list[winner_index]
 
+    #New placeholder list to hold percentages of votes for each candidate that will be calculated next.
     percentages = []
 
+    #Loop to calculate percent of votes for each candidate and format as a percentage with a decimal place.
+    #The percentages list is appended with the resulting values.
     for k in range(0, len(count_list)):
         percent = round((count_list[k] / total_votes) * 100, 2)
         percentages.append(percent)
 
+    #Prints results. Need loop to print names of each candidate with required information.
+    #The loop allows me to soft code. Yay.
     print("Election Results")
     print("---------------------------")
     print(f'Total Votes: {total_votes}')
